6223d1652e8d564d6881641e7caf5513a80551fd
Till Hoffmann <till@spotify.com>
2020-05-12 15:03:11 +0100

    Prior to Nick's comments.

---
+++ uncited.py
@@ -0,0 +1,20 @@
+import argparse
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument('bib')
+parser.add_argument('aux')
+args = parser.parse_args()
+
+with open(args.bib) as fp:
+    text = fp.read()
+    available_keys = set(key.strip() for key in re.findall('@\w+{([^-].*?),', text))
+
+with open(args.aux) as fp:
+    text = fp.read()
+    used_keys = set(key.strip() for key in re.findall(r'\\abx@aux@cite\{(.*?)\}', text))
+
+print(f'found {len(available_keys)} available keys')
+print(f'found {len(used_keys)} used keys')
+print(f'unused keys:')
+print('\n'.join(sorted(available_keys - used_keys)))
